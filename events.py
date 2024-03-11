32bca58ca702b6619a3226a9e548a6b3935e8197
mansurbek046 <mansurbek046@gmail.com>
2024-03-11 17:04:22 +0500

    .

--- events.py
+++ events.py
@@ -349,31 +349,31 @@ async def event_controller(client, callback_query, app):
 
         case 'playlist':
             # Downloading playlist
-            try:
-                playlist_url='https://youtube.com/playlist?list='
-                playlist = Playlist(playlist_url+callback_data[2])
-                chat_id=callback_query.message.chat.id
-                if user.premium is None:
-                    await client.send_message(chat_id=chat_id, text=user_language['pl_buy_premium'])
-                    return None
-                await callback_query.message.delete()
-                downloading=await client.send_message(chat_id=chat_id, text=user_language['pl_downloading'])
+            # try:
+            playlist_url='https://youtube.com/playlist?list='
+            playlist = Playlist(playlist_url+callback_data[2])
+            chat_id=callback_query.message.chat.id
+            if user.premium is None:
+                await client.send_message(chat_id=chat_id, text=user_language['pl_buy_premium'])
+                return None
+            await callback_query.message.delete()
+            downloading=await client.send_message(chat_id=chat_id, text=user_language['pl_downloading'])
 
-                if callback_data[1] == "mp4":
-                    for video in playlist.videos:
-                        await download_playlist_video_async(video, user_language, callback_query, app, CHANNEL_ID, uploader, chat_id)
+            if callback_data[1] == "mp4":
+                for video in playlist.videos:
+                    await download_playlist_video_async(video, user_language, callback_query, app, CHANNEL_ID, uploader, chat_id)
 
-                elif callback_data[1] == "mp3":
-                    for video in playlist.videos:
-                        await download_playlist_audio_async(video, app, chat_id, CHANNEL_ID, on_complete, callback_query, uploader)
+            elif callback_data[1] == "mp3":
+                for video in playlist.videos:
+                    await download_playlist_audio_async(video, app, chat_id, CHANNEL_ID, on_complete, callback_query, uploader)
 
-                await app.delete_messages(chat_id, downloading.id)
-                await client.send_message(chat_id=callback_query.message.chat.id, text=user_language['completed'])
+            await app.delete_messages(chat_id, downloading.id)
+            await client.send_message(chat_id=callback_query.message.chat.id, text=user_language['completed'])
 
-            except Exception as e:
-                print(f"An error occurred while downloading playlist: {e}")
-                await app.delete_messages(chat_id, downloading.id)
-                await client.send_message(chat_id=callback_query.message.chat.id, text=user_language['err_playlist_download'])
+            # except Exception as e:
+                # print(f"An error occurred while downloading playlist: {e}")
+                # await app.delete_messages(chat_id, downloading.id)
+                # await client.send_message(chat_id=callback_query.message.chat.id, text=user_language['err_playlist_download'])
 
         case 'buy':
             try:
