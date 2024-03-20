3471fb63816a194285e948ff80cac07ec4a664f2
mansurbek046 <mansurbek046@gmail.com>
2024-03-20 05:11:37 +0500

    *

--- uploader.py
+++ uploader.py
@@ -26,7 +26,7 @@ async def upload_to_telegram(app, file_path, file_type, youtube_id, chat_id, res
             Video.create(id=video.id, youtube_id=youtube_id, resolution=resolution, video_type="mkv")
         else:
             Video.create(id=video.id, youtube_id=youtube_id, resolution=resolution, video_type="mp4")
-        send=await app.forward_messages(chat_id=chat_id, from_chat_id=CHANNEL_ID, message_ids=video.id)
+        send=await vido.forward(chat_id=chat_id,  message_idss=video.id, reply_markup=reply_markup)
 
         if send:
             os.remove(file_path)
