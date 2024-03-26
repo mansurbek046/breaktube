f9cae58632e2dbc8fde43a1e7936017602e90b3a
mansurbek046 <mansurbek046@gmail.com>
2024-03-26 20:51:02 +0500

    .

--- events.py
+++ events.py
@@ -289,7 +289,11 @@ async def event_controller(client, callback_query, app):
                     chat_id=callback_query.message.chat.id
 
                     if matching_records.exists():
-                        await app.forward_messages(chat_id=callback_query.message.chat.id, from_chat_id=CHANNEL_ID, message_ids=int(str(matching_records.first().id)))
+                        message_id=int(str(matching_records.first().id))
+                        message = app.get_messages(chat_id=CHANNEL_ID, message_ids=message_id)
+                        video_name=message.html_text.split(">")[1].split("<")[0]
+                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔍', switch_inline_query_current_chat=video_name)]])
+                        await app.copy_message(callback_query.message.chat.id, CHANNEL_ID, message_id)
                         await callback_query.message.delete()
                         os.remove(f'Keyboards/{chat_id}_back_keyboard.pkl')
                         
